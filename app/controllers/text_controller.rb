f2328873bc88080548018aa1a31e74daac60a41f
Neil Basu <nbasu02@gmail.com>
2012-12-16 21:06:42 -0800

    Donors cannot see list of claims or make claims; can see individual claims

--- app/controllers/text_controller.rb
+++ app/controllers/text_controller.rb
@@ -10,12 +10,10 @@ class TextController < ApplicationController
         :action => 'create',
         #Insert token below, which may be SMS or voice.  It will correspond to a project on your
         #tropo account
-        :token => '',
+        :token => '183571955e6e5945ac4a37c8762c5e928af42bccdc187a2df99051b259052fb68a9afc8d85ccaa99acf25eae',
         #Insert number with the +1 in front of it and message below
-        :phonenumber => "",
-        :message => "All page properties for Writer text documents, like for example the page orientation, are defined by page styles.
-By default, a new text document uses the 'Default' page style for all pages.
-If you open an existing text document, different page styles may have been applied to the different pages."
+        :phonenumber => "+12019257712",
+        :message => "All"
     }
     }
   end
